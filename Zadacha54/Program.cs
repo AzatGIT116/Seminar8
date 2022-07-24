118ae6b084778681adc4aa1287b21a29776ab868
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-25 00:49:42 +0300

    add Zadacha 54

---
+++ Zadacha54/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива. */
+
+Console.Clear();
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write(" | ");
+            Console.Write($"{matrix[i, j],2} | ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void DescendingStringInMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] array = GetMatrix(3, 4, 0, 10);
+Console.WriteLine("Исходный массив: ");
+Console.WriteLine();
+PrintMatrix(array);
+Console.WriteLine();
+Console.WriteLine("Отсортированные по убыванию строки массива: ");
+Console.WriteLine();
+DescendingStringInMatrix(array);
+PrintMatrix(array); 
